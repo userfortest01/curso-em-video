933d892e77623ee6f5d2018feb11fabadca82204
flameastro <flame77ofc5@gmail.com>
2025-09-21 20:56:53 -0300

    Interatividade Divertida
    Deixei a interatividade com o script do botão mais divertida e amigável

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 const button = document.querySelector("button")
+let contador = 0
+let cor = "blue"
 
-button.addEventListener('click', () => {
-    alert("Você clicou em mim!")
-})
+
+function verificaCor() {
+    if (cor === "blue") {
+        button.style.backgroundColor = "blue"
+        button.style.color
+        cor = "vermelho"
+        button.innerText = "Que tal clicar denovo???"
+    } else if (cor === "vermelho") {
+        button.style.backgroundColor = "red"
+        cor = "blue"
+        button.innerText = "E de novo?"
+    }
+}
+
+
+function adicionaAnimacao() {
+    button.addEventListener('click', () => {
+        if (contador >= 10) {
+            button.innerText = "Ok, chega de clicar!"
+            button.style.backgroundColor = "grey"
+        } else {
+            verificaCor()
+            ++contador
+            console.log(`Você clicou ${contador} vezes.`)
+        }
+    })
+}
+
+
+adicionaAnimacao()
